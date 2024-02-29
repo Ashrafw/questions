5a34542006ef9ecaa0cc8678755ebb3a54c3f551
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2024-02-29 22:11:32 +0000

    text colour changes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -9,7 +9,7 @@ export default function Home({}: Props) {
       <div className="flex flex-col items-center justify-center py-12 gap-4">
         <div className=" text-center">
           <h1 className="font-bold text-6xl px-4 text-center mb-8 max-sm:text-5xl">
-            Welcome to Med<span className=" text-blue-700">Express</span>
+            Welcome to Med<span className=" text-sky-700">Express</span>
           </h1>
           <h2 className="font-bold text-3xl  max-sm:text-2xl px-4 text-center  text-gray-600">
             {" "}
@@ -21,7 +21,7 @@ export default function Home({}: Props) {
         </div>
         <Link
           to={"/questions/Questionnaire"}
-          className="py-2 mt-10 px-6 shadow font-semibold rounded bg-blue-700 hover:bg-blue-800 text-white"
+          className="py-2 mt-10 px-6 shadow font-semibold rounded bg-sky-700 hover:bg-sky-800 text-white"
         >
           Start Consultation
         </Link>
